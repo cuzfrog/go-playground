8da470b36b9a95b02496a671b3ac46824155ce06
Cause Chung <cuzfrog@139.com>
2018-07-28 20:45:18 +0800

    Create baseUnion to reuse code

--- unionfind/quickunion.go
+++ unionfind/quickunion.go
@@ -1,37 +1,29 @@
 package unionfind
 
 type quUnions struct {
-	comCnt int
-	sites []int
+	baseUnions
 }
 
 func newQuUnions(n int) unions{
-	return &quUnions{n, genSites(n)}
-}
+	u := baseUnions{n, genSites(n), nil, nil}
 
-func (u *quUnions) union(a, b int) {
-	roota := u.find(a)
-	rootb := u.find(b)
-	if roota == rootb {
-		return
+	u.findImpl = func(a int) int {
+		for u.sites[a] != a {
+			a = u.sites[a]
+		}
+		return a
 	}
 
-	u.sites[roota] = rootb
-	u.comCnt--
-}
+	// find searches through links and returns the component root number
+	u.unionImpl = func(a int, b int) {
+		roota := u.find(a)
+		rootb := u.find(b)
+		if roota == rootb {
+			return
+		}
 
-// find searches through links and returns the component root number
-func (u *quUnions) find(a int) int {
-	for u.sites[a] != a {
-		a = u.sites[a]
+		u.sites[roota] = rootb
+		u.comCnt--
 	}
-	return a
-}
-
-func (u *quUnions) connected(a, b int) bool {
-	return u.find(a) == u.find(b)
-}
-
-func (u *quUnions) count() int {
-	return u.comCnt
+	return &quUnions{u}
 }
--- unionfind/unionfind.go
+++ unionfind/unionfind.go
@@ -3,13 +3,38 @@ package unionfind
 type unions interface {
 	//union connects two sites
 	union(a, b int)
-	//find returns component id for a given site
-	find(a int) int
 	//connected checks if two sites are in the same union
 	connected(a, b int) bool
 	//count returns the number of components
 	count() int
+
+	find(a int) int
 }
+
+type baseUnions struct {
+	comCnt    int
+	sites     []int
+	findImpl  func(int) int
+	unionImpl func(int, int)
+}
+
+func (u *baseUnions) union(a, b int) {
+	u.unionImpl(a, b)
+}
+
+func (u *baseUnions) find(a int) int {
+	return u.findImpl(a)
+}
+
+func (u *baseUnions) connected(a, b int) bool {
+	return u.findImpl(a) == u.findImpl(b)
+}
+
+func (u *baseUnions) count() int {
+	return u.comCnt
+}
+
+/* --- Utility functions --- */
 
 func genSites(n int) []int {
 	sites := make([]int, n, n)
