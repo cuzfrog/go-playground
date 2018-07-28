dca73b65a37e459d417a6b64d1a063c010d54e4b
Cause Chung <cuzfrog@139.com>
2018-07-28 21:07:01 +0800

    Embedding for wquUnions

--- unionfind/weightedQuickunion.go
+++ unionfind/weightedQuickunion.go
@@ -1,46 +1,39 @@
 package unionfind
 
 type wquUnions struct {
-	comCnt int
-	sites  []int
+	*abstractUnions
 }
 
 func newWquUnions(n int) unions {
-	return &wquUnions{n, genSites(n)}
-}
+	u := &abstractUnions{n, genSites(n), nil, nil}
 
-func (u *wquUnions) union(a, b int) {
-	rootA, dpthA := u.findDepth(a)
-	rootB, dpthB := u.findDepth(b)
-	if rootA == rootB {
-		return
+	u.findImpl = func(a int) int {
+		comNum, _ := u.findDepth(a)
+		return comNum
 	}
-	if dpthA <= dpthB {
-		u.sites[rootA] = rootB
-	} else {
-		u.sites[rootB] = rootA
-	}
-	u.comCnt--
-}
 
-func (u *wquUnions) find(a int) int {
-	comNum, _ := u.findDepth(a)
-	return comNum
+	u.unionImpl = func(a int, b int) {
+		rootA, dpthA := u.findDepth(a)
+		rootB, dpthB := u.findDepth(b)
+		if rootA == rootB {
+			return
+		}
+		if dpthA <= dpthB {
+			u.sites[rootA] = rootB
+		} else {
+			u.sites[rootB] = rootA
+		}
+		u.comCnt--
+	}
+
+	return &wquUnions{u}
 }
 
 // find searches through links and returns the component root number and tree depth
-func (u *wquUnions) findDepth(a int) (int, depth int) {
+func (u *abstractUnions) findDepth(a int) (int, depth int) {
 	for u.sites[a] != a {
 		a = u.sites[a]
 		depth++
 	}
 	return a, depth
-}
-
-func (u *wquUnions) connected(a, b int) bool {
-	return u.find(a) == u.find(b)
-}
-
-func (u *wquUnions) count() int {
-	return u.comCnt
 }
