1d0e4ad81c98f9769cb79696572cf9b61eba127a
Cause Chung <cuzfrog@139.com>
2018-08-22 21:31:45 +0800

    add benchmark and refine test

--- sorting/insertion.go
+++ sorting/insertion.go
@@ -4,7 +4,7 @@ func insertionSort(a []int) []int {
 	n := len(a)
 	for i := 1; i < n; i++ {
 		for j := i; j > 0 && a[j] < a[j-1]; j-- {
-			exchange(a, i, j)
+			exchange(a, j, j - 1)
 		}
 	}
 	return a
--- sorting/sorting_test.go
+++ sorting/sorting_test.go
@@ -6,12 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func test(f func([]int) []int, t *testing.T) {
+	a := genElems(32)
+	t.Log("Before:", a)
+	s := f(a)
+	t.Log("Sorted:", s)
+	checkSorted(s, t)
+}
+
+func TestSorting(t *testing.T) {
+	t.Run("insertion sort", func(t *testing.T) { test(insertionSort, t) })
+}
+
 func checkSorted(a []int, t *testing.T) {
 	n := len(a)
 	for i := 1; i < n; i++ {
 		e1 := a[i-1]
 		e2 := a[i]
-		b := e1 < e2
+		b := e1 <= e2
 		assert.Truef(t, b, "at index %d, elem %d is smaller than its previous elem %d", i, e1, e2)
 		if !b {
 			t.FailNow()
@@ -21,14 +33,29 @@ func checkSorted(a []int, t *testing.T) {
 
 func genElems(n int) []int {
 	a := make([]int, n, n)
+	max := n * 4
 	for i := range a {
-		a[i] = rand.Intn(2048)
+		a[i] = rand.Intn(max)
 	}
 	return a
 }
 
-func TestInsertionSort(t *testing.T) {
-	a := genElems(1000)
-	insertionSort(a)
-	checkSorted(a, t)
+func benchmark(f func([]int) []int, n int, b *testing.B) {
+	a := genElems(n)
+	as := make([][]int, b.N, b.N)
+	for i := range as {
+		as[i] = make([]int, n, n)
+		copy(as[i], a)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		f(as[i])
+	}
+}
+
+func Benchmark_InsertionSort(b *testing.B) {
+	f := insertionSort
+	b.Run("100", func(b *testing.B) { benchmark(f, 100, b) })
+	b.Run("1000", func(b *testing.B) { benchmark(f, 1000, b) })
+	b.Run("10000", func(b *testing.B) { benchmark(f, 10000, b) })
 }
