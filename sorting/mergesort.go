7f9c549b72d133a96cc9c63224fdd7c3b6ad2983
Cause Chung <cuzfrog@139.com>
2018-08-29 16:14:11 +0800

    add buf to merge sort

--- sorting/mergesort.go
+++ sorting/mergesort.go
@@ -1,25 +1,51 @@
 package sorting
 
-func mergesort(a []int) []int {
+// allocate aux array on every call
+func mergesort(a []int) {
 	n := len(a)
 	if n > 1 {
 		mid := n / 2
 		a1 := a[:mid]
-		a2 := a[mid+1:]
-		return merge(mergesort(a1), mergesort(a2))
-	} else {
-		return a
+		a2 := a[mid:]
+		mergesort(a1)
+		mergesort(a2)
+		merge(a1, a2)
 	}
 }
 
-func merge(a1, a2 []int) []int {
+// use a buf to avoid make new arr
+func mergesortBuf(a []int) {
+	buf := make([]int, len(a))
+	mergesortWithBuffer(a, buf)
+}
+
+func mergesortWithBuffer(a []int, buf []int) {
+	n := len(a)
+	if n > 1 {
+		mid := n / 2
+		a1 := a[:mid]
+		a2 := a[mid:]
+		mergesortWithBuffer(a1, buf)
+		mergesortWithBuffer(a2, buf)
+		mergeWithBuffer(a1, a2, buf)
+	}
+}
+
+func merge(a1, a2 []int) {
 	l1 := len(a1)
 	l2 := len(a2)
 	aux := make([]int, l1+l2)
+	mergeWithBuffer(a1, a2, aux)
+}
+
+func mergeWithBuffer(a1, a2 []int, buf []int) {
+	l1 := len(a1)
+	l2 := len(a2)
+	n := l1 + l2
+	aux := buf[:n]
 
 	i1 := 0
 	i2 := 0
-
 	for j := range aux {
 		if i1 >= l1 {
 			aux[j] = a2[i2]
@@ -35,20 +61,23 @@ func merge(a1, a2 []int) []int {
 			i2++
 		}
 	}
-	return aux
+
+	a := a1[:n] //contract: a1, a2 are consecutive
+	copy(a, aux)
 }
 
-func mergesortFromBottom(a []int) []int {
+func mergesortFromBottom(a []int) {
 	n := len(a)
+	b := make([]int, n)
+	copy(b, a)
+
 	for sz := 1; sz < n; sz *= 2 {
 		for i := 0; i < n-sz; i += sz * 2 {
 			mid := i + sz
 			end := min(i+sz*2, n)
 			a1 := a[i:mid]
 			a2 := a[mid:end]
-			m := merge(a1, a2)
-			copy(a[i:end], m)
+			mergeWithBuffer(a1, a2, b)
 		}
 	}
-	return a
 }
