289ee212370c612ea09b814575f8d88c6e7f2396
Cause Chung <cuzfrog@139.com>
2018-09-01 22:55:10 +0800

    impl heapsort sink

--- sorting/heapsort.go
+++ sorting/heapsort.go
@@ -1,5 +1,35 @@
 package sorting
 
-func heapSort(a []int)  {
+func heapSort(a []int) {
+	n := len(a)
+	if n <= 1 {
+		return
+	}
 
+	for k := n / 2; k >= 1; k-- {
+		sink(a, k)
+	}
+	for i := n - 1; i >= 0; i++ {
+		Exch(a, 0, i)
+
+	}
+}
+
+// sink tries to heapify an array 'a' by sinking elem at index 'k-1'.
+//  Contract: 1 <= k <= len(a)/2, so node a[k-1] has at least one child.
+func sink(a []int, k int) {
+	n := len(a)
+	s := k*2 + 1
+	for s-1 <= n { //at least left node exists
+		if s > n || a[s-2] > a[s-1] { // only left child || left > right
+			s--
+		}
+		if a[s-1] > a[k-1] {
+			Exch(a, k-1, s-1)
+			k = s
+			s = k*2 + 1
+		} else {
+			break
+		}
+	}
 }
--- sorting/heapsort_test.go
+++ sorting/heapsort_test.go
@@ -1,1 +1,16 @@
 package sorting
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_heapSort_sink(t *testing.T) {
+	a := []int{3, 5, 8, 2, 5, 9}
+	sink(a, 3)
+	assert.Equal(t, []int{3, 5, 9, 2, 5, 8}, a)
+	sink(a, 2)
+	assert.Equal(t, []int{3, 5, 9, 2, 5, 8}, a)
+	sink(a, 1)
+	assert.Equal(t, []int{9, 5, 8, 2, 5, 3}, a)
+}
