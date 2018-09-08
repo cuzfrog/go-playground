d0f63f2b2469d07baf34105e11c72d2f2b6555f2
Cause Chung <cuzfrog@139.com>
2018-09-08 11:07:10 +0800

    test splitNode3

--- searching/23tree_test.go
+++ searching/23tree_test.go
@@ -19,24 +19,33 @@ func Test_upgradeLeafNode2(t *testing.T) {
 	assert.Equal(t, -1, n2.e.k)
 }
 
-func Test_splitRootNode3(t *testing.T) {
+func Test_splitNode3(t *testing.T) {
 	n1 := newNode3(2, "s", 5, "gg", nil)
 	l := &entry{1, "left entry"}
-	splitNode3(n1, nil, l)
-	assert.Equal(t, 2, n1.e.k)
+	nr1, eu1 := splitNode3(n1, nil, l)
+	assert.Equal(t, 1, n1.e.k)
 	assert.False(t, n1.is3)
-	assert.Equal(t, 1, n1.left.e.k)
-	assert.Equal(t, 5, n1.right.e.k)
+	assert.Nil(t, n1.mid)
+	assert.Nil(t, n1.er)
+	assert.Equal(t, 2, eu1.k)
+	assert.Equal(t, 5, nr1.e.k)
+
+	left, mid, right, cr := &node23{}, &node23{}, &node23{}, &node23{}
 
 	n2 := newNode3(2, "s2", 5, "gg3", nil)
+	n2.left, n2.mid, n2.right = left, mid, right
 	m := &entry{3, "mid entry"}
-	splitNode3(n2, nil, m)
-	assert.Equal(t, 3, n2.e.k)
+	nr2, _ := splitNode3(n2, cr, m)
+	assert.Equal(t, 2, n2.e.k)
+	assert.Equal(t, left, n2.left)
+	assert.Equal(t, mid, n2.right)
+	assert.Equal(t, cr, nr2.left)
+	assert.Equal(t, right, nr2.right)
 
 	n3 := newNode3(2, "s3", 5, "gg3g", nil)
 	r := &entry{8, "right entry"}
 	splitNode3(n3, nil, r)
-	assert.Equal(t, 5, n3.e.k)
+	assert.Equal(t, 2, n3.e.k)
 }
 
 func newNode2(k int, v interface{}, p *node23) *node23 {
