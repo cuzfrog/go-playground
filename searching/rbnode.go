41997e64624e3f08580ff3af64e435883502362e
Cause Chung <cuzfrog@139.com>
2018-09-16 16:34:45 +0800

    impl and test rbnode insert

--- searching/rbnode.go
+++ searching/rbnode.go
@@ -15,6 +15,26 @@ const (
 	black color = false
 )
 
+/* ----------------- functions ----------------- */
+
+// return top node and old value
+func (n *rbnode) insert(k int, v interface{}) (*rbnode, interface{}) {
+	var old interface{}
+	if n == nil { //empty, create single black
+		n = &rbnode{k: k, v: v, c: red}
+	} else if k == n.k {
+		old, n.v = n.v, v
+	} else if k < n.k {
+		n.left, old = n.left.insert(k, v)
+	} else { //k > n.k
+		n.right, old = n.right.insert(k, v)
+	}
+	n = checkToFlipColorOrRotate(n)
+	return n, old
+}
+
+/* ----------------- utils ----------------- */
+
 /*     1b         2b
       /  \        / \
      a  2r      1r   d
@@ -22,12 +42,12 @@ const (
       c   d    a  c
  */
 func rotateLeft(n *rbnode) *rbnode {
-	if n.right == nil{
+	if n.right == nil {
 		return n
 	}
 	r := n.right
 	n.right, r.left = r.left, n
-	n.c, r.c = red, black
+	n.c, r.c = r.c, n.c
 	return r
 }
 
@@ -38,11 +58,49 @@ func rotateLeft(n *rbnode) *rbnode {
       a   c       c  d
  */
 func rotateRight(n *rbnode) *rbnode {
-	if n.left == nil{
+	if n.left == nil {
 		return n
 	}
 	l := n.left
 	n.left, l.right = l.right, n
-	n.c, l.c = red, black
+	n.c, l.c = l.c, n.c
 	return l
+}
+
+func (n *rbnode) isBlack() bool {
+	return n == nil || n.c == black
+}
+
+func checkToFlipColorOrRotate(n *rbnode) *rbnode {
+	if n.left.isBlack() && n.right.isBlack() {
+		//do nothing
+	} else if n.right.isBlack() && n.left.c == red {
+		if !n.left.left.isBlack() { // double red on left branch
+			n = rotateRight(n)
+		}
+	} else if n.left.isBlack() && n.right.c == red {
+		n = rotateLeft(n)
+	}
+	if !n.left.isBlack() && !n.right.isBlack() { //both l,r are red
+		n.left.c, n.right.c = black, black
+		n.c = red
+	}
+	return n
+}
+
+func insertToLeftRed(n *rbnode, k int, v interface{}) (*rbnode, interface{}) {
+	var old interface{}
+	l := n.left
+	if k == l.k {
+		old = l.v
+		l.v = v
+	} else if k < n.left.k {
+		l.left = &rbnode{k: k, v: v, c: red}
+		n = rotateRight(n)
+	} else {
+		l.right = &rbnode{k: k, v: v, c: red}
+		n.left = rotateLeft(l)
+		n = rotateRight(n)
+	}
+	return n, old
 }
--- searching/rbnode_test.go
+++ searching/rbnode_test.go
@@ -5,6 +5,91 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_insert(t *testing.T) {
+	asert := assert.New(t)
+	n := &rbnode{k: 10, v: "10"}
+	nn, old := n.insert(5, "5")
+	asert.Equal(nn, n)
+	asert.Nil(old)
+	asert.Equal(5, n.left.k)
+	asert.Equal(red, n.left.c)
+	asert.Equal(black, n.c)
+	/*   10b
+	     /
+	    5r
+	 */
+	_, old = n.insert(5, "5a")
+	asert.Equal("5", old)
+	asert.Equal("5a", n.left.v)
+	_, old = n.insert(10, "10a")
+	asert.Equal("10", old)
+	asert.Equal("10a", n.v)
+
+	n.insert(11, "11")
+	asert.Equal(black, n.left.c)
+	asert.Equal(black, n.right.c)
+	asert.Equal(red, n.c)
+	asert.Equal(11, n.right.k)
+	/*   10r
+	    /  \
+	   5b  11b
+	 */
+	_, old = n.insert(11, "11a")
+	asert.Equal("11", old)
+	asert.Equal("11a", n.right.v)
+
+	n.insert(8, "8")
+	asert.Equal(8, n.left.k)
+	asert.Equal(5, n.left.left.k)
+	asert.Equal(red, n.left.left.c)
+	/*   10r         10r
+	    /  \        /   \
+	   5b  11b     8b   11b
+	    \         /
+	     8r     5r
+	 */
+
+	n.insert(6, "6")
+	asert.Equal(red, n.left.c)
+	asert.Equal(black, n.left.left.c)
+	asert.Equal(black, n.left.right.c)
+	asert.Equal(6, n.left.k)
+	asert.Equal(5, n.left.left.k)
+	asert.Equal(8, n.left.right.k)
+	/*  10r         10r            10r
+	   /  \        /   \          /   \
+	  8b  11b     8b   11b       6r   11b
+	 /           /             /   \
+	5r          6r            5b   8b
+	 \         /
+	  6r      5r
+	*/
+
+	n.insert(2, "2")
+	n, _ = n.insert(1, "1")
+	asert.Equal(6, n.k)
+	asert.Equal(red, n.c)
+	asert.Equal(2, n.left.k)
+	asert.Equal(black, n.left.c)
+	asert.Equal(1, n.left.left.k)
+	asert.Equal(black, n.left.left.c)
+	asert.Equal(10, n.right.k)
+	asert.Equal(black, n.right.c)
+	asert.Equal(8, n.right.left.k)
+	asert.Equal(black, n.right.left.c)
+	asert.Equal(11, n.right.right.k)
+	asert.Equal(black, n.right.right.c)
+
+	/*    10r          6r
+	     /  \        /   \
+	    6r  11b     2b   10b
+	   /  \        / \   / \
+	  2r   8b     1b 5b 8b 11b
+	 / \
+	1b 5b
+	 */
+}
+
 func Test_rotateLeft(t *testing.T) {
 	asert := assert.New(t)
 	n := &rbnode{k: 1}
